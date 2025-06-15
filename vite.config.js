a0c17a765f3f19cc4679fa6fb68d7989e7e6150d
Rakshan001 <rakshanshetty2003@gmail.com>
2025-06-16 00:53:30 +0530

    second comit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: '[name].[hash].mjs',
+        chunkFileNames: '[name].[hash].mjs',
+        assetFileNames: '[name].[hash][extname]'
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'text/javascript'
+    }
   }
 })
